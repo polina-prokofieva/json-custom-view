a22ed0128711647114a7ed0d6ba02dfd41d38d4b
Polina Prokofieva <polina.prokofieva@gmail.com>
2023-01-02 16:26:05 +0200

    Render whole data

--- src/render.js
+++ src/render.js
@@ -20,29 +20,61 @@ const createSimpleDOMElement = (tag, value = '', classNameOrOptions) => {
   return element;
 };
 
-const renderSimpleValue = (key, value) => {
+const toggleVisibility = objectElement => {
+  objectElement.classList.toggle(styles.opened);
+
+  if (!objectElement.classList.contains(styles.opened)) {
+    const allOpenedChildren = objectElement.querySelectorAll(
+      `.${styles.opened}`
+    );
+
+    allOpenedChildren.forEach(element => {
+      element.classList.remove(styles.opened);
+    });
+  }
+};
+
+const renderObject = (data, className = styles.main) => {
+  const mainElement = createSimpleDOMElement('div', null, className);
+
+  for (const key in data) {
+    const value = data[key];
+    const type = Array.isArray(value) ? 'array' : typeof value;
+
+    const objectElement = createSimpleDOMElement('div', null, [
+      styles.field,
+      styles[type],
+    ]);
+
+    const { keyElement, fragment } = renderField(key, value);
+
+    objectElement.appendChild(fragment);
+    keyElement.addEventListener('click', () => toggleVisibility(objectElement));
+
+    mainElement.appendChild(objectElement);
+  }
+
+  return mainElement;
+};
+
+const renderField = (key, value) => {
   const fragment = document.createDocumentFragment();
+  const keyElement = createSimpleDOMElement('span', key, styles.key);
 
-  fragment.appendChild(createSimpleDOMElement('span', key, styles.key));
-  fragment.appendChild(createSimpleDOMElement('span', ': ', styles.value));
-  fragment.appendChild(createSimpleDOMElement('span', value, styles.value));
+  fragment.appendChild(keyElement);
 
-  return fragment;
+  if (typeof value === 'object') {
+    fragment.appendChild(renderObject(value, styles.value));
+  } else {
+    fragment.appendChild(createSimpleDOMElement('span', ': ', styles.value));
+    fragment.appendChild(createSimpleDOMElement('span', value, styles.value));
+  }
+
+  return { keyElement, fragment };
 };
 
 export const render = (convertedData, rootElement) => {
-  const mainElement = createSimpleDOMElement('div', null, styles.main);
-
-  for (const key in convertedData) {
-    const value = convertedData[key];
-    const objectElement = createSimpleDOMElement('div', null, [
-      styles.field,
-      styles[typeof value],
-    ]);
-
-    objectElement.appendChild(renderSimpleValue(key, value));
-    mainElement.appendChild(objectElement);
-  }
+  const mainElement = renderObject(convertedData);
 
   rootElement.appendChild(mainElement);
 };
