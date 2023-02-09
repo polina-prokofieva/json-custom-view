d3cf43c7df28644743b7063c313c3982f760a654
Polina Prokofieva <polina.prokofieva@gmail.com>
2023-02-09 10:58:42 +0200

    Added example files

---
+++ main.js
@@ -0,0 +1,35 @@
+// import { dataExample as JSONdata } from './data/big';
+import { responseJson } from './tests/data/response';
+// import { defaultSettings as settings } from './tests/data/settings';
+import { generate, transform } from './src/api.js';
+// import { data } from './data/realDataRestricted';
+
+const root = document.querySelector('#main');
+const data = {
+  Bespoke: {
+    ToBeDefined: [
+      {
+        name: 'Harry',
+        surname: 'Potter',
+      },
+      {
+        name: 'Hermiona',
+        surname: 'Grainger',
+      },
+    ],
+  },
+  somevalue: null,
+};
+
+const settings = {
+  isFormatKeys: true,
+  boolAppearence: ['Yes', 'No'],
+  // nullAppearence: '---',
+  hideEmpty: true,
+  isMergeSingleFields: true,
+  showNotifications: true,
+  arraysAsTable: ['ToBeDefined', 'trades', 'inquiries'],
+};
+
+generate(data, root, settings);
+console.log(transform(data, settings));
