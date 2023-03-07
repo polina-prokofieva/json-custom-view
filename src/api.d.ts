f3da1cf88a792d795cdd9d2f3f7baddacc820a29
Polina Prokofieva <polina.prokofieva@gmail.com>
2023-03-07 11:01:01 +0200

    add declaration and update scripts

---
+++ src/api.d.ts
@@ -0,0 +1,43 @@
+declare module 'json-custom-view' {
+  export type ValueType =
+    | string
+    | number
+    | boolean
+    | null
+    | ValueType[]
+    | ObjectValueType;
+
+  export type ObjectValueType = { [key: string]: ValueType };
+
+  export interface keysForArraysType {
+    [key: string]: string;
+  }
+
+  export type RootType = string | string[];
+
+  export interface SettingsType {
+    [key: string]: any;
+    root?: RootType;
+    isFormatKeys?: boolean;
+    nullAppearence?: string;
+    boolAppearence?: [string, string];
+    hidePropertiesByValue?: string[];
+    hidePropertiesByKey?: string[];
+    hideEmpty?: boolean;
+    isMergeSingleFields?: boolean;
+    keysForArrays?: keysForArraysType;
+    arraysAsTable?: string[];
+    showNotifications?: boolean;
+  }
+
+  export const generate: (
+    data: ValueType,
+    nodeElement: HTMLElement,
+    customSettings?: SettingsType
+  ) => void;
+
+  export const transform: (
+    data: ValueType,
+    customSettings?: SettingsType
+  ) => object;
+}
